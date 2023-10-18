a606a97a89dfc19bc239326fa0c188823d192e80
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-10-18 12:15:38 +0300

    ex 3 was done

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;	
+
+int main() {
+	cout << "Puppies for sale." << endl;
+	cout << "Breed: Rottweiler.\n" << endl;
+	cout << "......\n" << endl;
+	cout << "Call: 222-22-22\n" << endl;
+	cout << "222" <<  "\t|  222  |" << endl;
+	cout << "22" << "\t|  22   |" << endl;
+	cout << "22" << "\t|  22   |\n" << endl;
+	cout <<"etc." << endl;
+	return 0;
+}
