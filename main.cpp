7ec1a5531b166bc0dee9e94091fb4d37d1bb5505
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-10-18 14:10:07 +0300

    ex 2 was done

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\tEvery hunter wants to know\n";
+    cout << "\t\twhere the others are,\n";
+    cout << "\t\tif you would have told me\n";
+    cout << "\t\t\tI would have had a different answer,\n";
+    cout << "\t\t\tnot the one you know.\n";
+    cout << "\tEveryone a mother,\n";
+    cout << "\t\teveryone a father,\n";
+    cout << "\t\teveryone a small animal\n";
+    cout << "\t\t\tin the grass.\n";
+    cout << "\tEverything is natural:\n";
+    cout << "\t\tthe barn is\n";
+    cout << "\t\ton fire,\n";
+    cout << "\t\tthe children are\n";
+    cout << "\t\teating,\n";
+    cout << "\t\tthe telephone is\n";
+    cout << "\t\ton the wall\n";
+    cout << "\t\t\tand it rings.\n";
+
+    return 0;
+}
+
