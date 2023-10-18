244df9280508ab4f7ffc5266acadbf947451804e
Maikruts Ilya Oleksandrovych <143324723+1337rave@users.noreply.github.com>
2023-10-18 12:30:41 +0300

    ex 1 was done

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Robert Frost\n" << endl;
+	cout << "The Road Not Taken\n" << endl;
+	cout << "Two roads diverged in a yellow wood" << endl;
+	cout << "I took the one less traveled by" << endl;
+	cout << "And that has made all the difference" << endl;
+	return 0;
+}
